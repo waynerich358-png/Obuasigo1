26038a69216f00a84522cf41381c91aff7e1cef2
waynerich358-png <waynerich358@gmail.com>
2026-09-17 11:54:53 +0000

    Add files via upload

---
+++ Public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('push',event=>{let data={title:'ObuasiGo',body:'You have an update'};try{data=event.data.json()}catch{}event.waitUntil(self.registration.showNotification(data.title,{body:data.body,icon:'/icon-192.png',badge:'/icon-192.png'}))});self.addEventListener('notificationclick',event=>{event.notification.close();event.waitUntil(clients.openWindow('/'))});
